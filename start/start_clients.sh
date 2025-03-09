a5394408ee34f67d8d47b6aae1f663281980fd1e
Alexander Yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2025-03-09 21:34:46 +0100

    simplified process

--- start/start_clients.sh
+++ start/start_clients.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-OK_NODES_PATH=.ok_client_nodes
+HOST_SITE=sophia
+NODES_CLIENT_FILE=~/.ok_nodes_client
 
-function start_client_nodes {
-    # where we initiate packet sending with stresstest tool
-
-    # arg1: client grid5000 site
-    # arg2: number of clients
-    # arg3: duration of holding hosts
-    # arg3: client environment path for kadeploy3
-
-    CLIENT_GRID5000_SITE=$1
-    NUM_CLIENTS=$2
-    DURATION=$3
-    CLIENT_ENV_PATH=$4
-
-
-    ssh -tt $CLIENT_GRID5000_SITE << EOF
-oarsub -I -t deploy -l host=$NUM_CLIENTS,walltime=$DURATION
-kadeploy3 $CLIENT_ENV_PATH --output-ok-nodes $OK_NODES_PATH
-EOF
-    echo "Client nodes are ready to be used"
-}
-
-start_client_nodes $CLIENT_GRID5000_SITE $NUM_CLIENTS $DURATION $CLIENT_ENV_PATH
+#OAR -t deploy
+kadeploy3 -a ~/mutilate-environment.yaml --output-ok-nodes $NODES_CLIENT_FILE
+# to make the container run forever until walltime is reached
+scp $NODES_CLIENT_FILE $HOST_SITE:$NODES_CLIENT_FILE
+ssh $HOST_SITE "oarsub -S ~/public/dont-throw-out-the-stack/start/start_host.sh"
+sleep infinity
--- start/start_host.sh
+++ start/start_host.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#OAR -t deploy
+#OAR -t destructive
+
 
 function get_current_interface_ip() {
     INTERFACES=$(ip link show | grep -v 'lo' | grep 'state UP' | awk '{print $2}' | tr -d ':')    
@@ -8,39 +11,15 @@ function get_current_interface_ip() {
     echo $IP
 }
 
-function update_client_ssh() {
-    # here we will install ssh keys for the client nodes into the experience node 
-    # so the experience node can ssh into the client nodes (for mutilate tool)
+HOST_PATH=~/.ok_exp_host
+CLIENTS_PATH=~/.ok_nodes_client
 
-    # arg1: path file of nodes (nodes will be seperated by new line)
-    # arg2: experience node
-    EXP_NODE=$(cat $OK_EXP_NODES_PATH | head -n 1)
-    NODES=$(cat $OK_NODES_CLIENT_PATH | tr "," "\n")
-    for NODE in $NODES;
-    do
-        ssh root@$NODE "ssh-keygen"
-        PUB_KEY=$(ssh root@$NODE "cat ~/.ssh/id_rsa.pub")
-        ssh root@$EXP_NODE "echo $PUB_KEY >> ~/.ssh/authorized_keys"
-    done
-}
-
-function start_host_node() {
-    # where we measure received packet performances
-    
-    # arg1: grid5000 site
-    # arg2: duration of holding host
-    # arg3: client environment path for kadeploy3
-    GRID5000_SITE=$1
-    DURATION=$2
-    HOST_ENV_PATH=$3
-
-    ssh -tt $GRID5000_SITE << EOF
-$(typeset -f update_client_ssh)
-oarsub -I -t deploy -l host=1,walltime=$DURATION
-kadeploy3 $HOST_ENV_PATH --output-ok-nodes $OK_EXP_NODES_PATH
-update_client_ssh $OK_NODES_PATH 
-EOF
-}
-
-
-start_host_node $HOST_GRID5000_SITE $DURATION $HOST_ENV_PATH
+kadeploy3 -a ~/ --output-ok-nodes $HOST_PATH
+EXP_NODE=$(cat $HOST_PATH | head -n 1)
+NODES=$(cat $CLIENTS_PATH | tr "," "\n")
+for NODE in $NODES;
+do
+    ssh root@$NODE "ssh-keygen"
+    PUB_KEY=$(ssh root@$NODE "cat ~/.ssh/id_rsa.pub")
+    ssh root@$EXP_NODE "echo $PUB_KEY >> ~/.ssh/authorized_keys"
+done
