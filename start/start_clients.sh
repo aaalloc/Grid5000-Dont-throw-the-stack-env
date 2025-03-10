6699a55dfd62067385a1381a9d721d003b537c63
Alexander Yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2025-03-10 10:21:19 +0100

    executable and -a for kadeploy

--- start/start_clients.sh
+++ start/start_clients.sh
@@ -2,10 +2,10 @@
 
 #OAR -t deploy
 
-HOST_SITE=sophia
+HOST_SITE=grenoble
 NODES_CLIENT_FILE=~/.ok_nodes_client
 
-kadeploy3 mutilate-environment.yaml --output-ok-nodes $NODES_CLIENT_FILE
+kadeploy3 -a mutilate-environment.yaml --output-ok-nodes $NODES_CLIENT_FILE
 scp $NODES_CLIENT_FILE $HOST_SITE:$NODES_CLIENT_FILE
 ssh $HOST_SITE "oarsub -S ~/public/dont-throw-the-stack/start/start_host.sh"
 # to make the container run forever until walltime is reached
