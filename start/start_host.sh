d42fbac9c22103a10b7cea5af77c76a51965b3ff
Alexander Yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2025-03-12 10:53:16 +0100

    updated environment conf and script

--- start/start_host.sh
+++ start/start_host.sh
@@ -20,7 +20,7 @@ kadeploy3 -a build/environment/environment.dsc --output-ok-nodes $HOST_NODE_PATH
 EXP_NODE=$(cat $HOST_NODE_PATH | head -n 1)
 NODES=$(cat $CLIENTS_NODES_PATH | tr "," "\n")
 # path here is hardcoded for simplicity, be carefull so that in netstack conf it corresponds (mutilate.sh)
-paste -sd,$CLIENTS_NODES_PATH  | ssh root@$EXP_NODE "cat > /tmp/mutilate_nodes"
+paste -sd, $CLIENTS_NODES_PATH  | ssh root@$EXP_NODE "cat > /tmp/mutilate_nodes"
 
 ssh root@$EXP_NODE "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y"
 PUB_KEY=$(ssh root@$EXP_NODE "cat ~/.ssh/id_rsa.pub")
